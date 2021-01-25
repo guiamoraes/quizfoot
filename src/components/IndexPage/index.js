2662c074a2ac3c786fb62e25b55a40a7eff9f6e5
Guilherme <guilherme.moraes@ima.sp.gov.br>
2021-01-25 20:23:29 -0300

    Url image content

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -13,12 +13,12 @@ function IndexPage() {
         <meta property="og:url" content="https://quizfoot.vercel.app/" />
         <meta property="og:title" content="Quiz Foot" />
         <meta property="og:description" content="Quiz Foot criado durante imersão Alura  React Next" />
-        <meta property="og:image" content="" />
+        <meta property="og:image" content="https://images5.alphacoders.com/387/387711.jpg" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://quizfoot.vercel.app/" />
         <meta property="twitter:title" content="Quiz Foot" />
         <meta property="twitter:description" content="Quiz Foot criado durante imersão Alura  React Next" />
-        <meta property="twitter:image" content=""></meta>
+        <meta property="twitter:image" content="https://images5.alphacoders.com/387/387711.jpg"></meta>
       </Head>
     </div>
   )
