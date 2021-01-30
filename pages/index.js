db670430a84ab6206cbc738ea5d11a4a200cce11
Guilherme <guilherme.moraes@ima.sp.gov.br>
2021-01-30 14:41:35 -0300

    atualiza index

--- pages/index.js
+++ pages/index.js
@@ -70,11 +70,11 @@ export default function Home() {
               <Input
                 name="nomeDoUsuario"
                 onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
-                placeholder="Diz ai seu nome"
+                placeholder="Qual seu nome? Jogou aonde?"
                 value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
-                {`Jogar ${name}`}
+                {`Jogar como ${name}`}
               </Button>
             </form>
           </Widget.Content>
